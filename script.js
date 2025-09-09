a7fa9cc6b617052736eb276cdcad5f878fb5752c
DFStechBox <dfosterstewart@gmail.com>
2025-09-09 15:18:00 -0700

    finished project

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let firstCard;
-let secondCard;
+// let firstCard;
+// let secondCard;
 
 
-function getRandomIntInclusive(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-}
+// function getRandomIntInclusive(min, max) {
+//   const minCeiled = Math.ceil(min);
+//   const maxFloored = Math.floor(max);
+//   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+// }
 
 // Example usage to get a random number between 2 and 11
-const randomNumber = getRandomIntInclusive(2, 11);
-console.log(randomNumber);
+// const randomNumber = getRandomIntInclusive(2, 11);
+// console.log(randomNumber);
+
+// sum = firstCard + secondCard;
+
+let user = 16;
+
+if (user < 21) {
+  console.log("You cannot enter this club");
+} else {
+  console.log("Welcome to the club");
+}
